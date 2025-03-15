762537ebd4df36eb9bacf3dd9cae2c5a93f5e08f
admin <admin@admins-MacBook-Pro.local>
2025-03-15 15:15:09 +0100

    commit changes

--- src/app/features/services/services.component.ts
+++ src/app/features/services/services.component.ts
@@ -1,6 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, Inject, inject, PLATFORM_ID  } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { Router } from '@angular/router';
 import * as AOS from 'aos';
+import { Carousel } from 'bootstrap';
 
 @Component({
   selector: 'app-services',
@@ -9,11 +11,24 @@ import * as AOS from 'aos';
   styleUrl: './services.component.scss'
 })
 export class ServicesComponent {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
   private router = inject(Router);
 
   ngOnInit(): void {
     AOS.init();
     AOS.refresh();
+  }
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const myCarousel = new (window as any).bootstrap.Carousel(document.getElementById('myCarousel')!, {
+        interval: 700,
+        ride: 'carousel',
+        wrap: true
+      });
+
+      myCarousel.cycle();
+    }
   }
 
   goToContact() {
