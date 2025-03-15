41745b469fb564ed29a7d34eb535b1bfd9dad3ef
admin <admin@admins-MacBook-Pro.local>
2025-03-15 17:05:21 +0100

    commit changes

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -28,32 +28,32 @@ export class HomeComponent {
 
   services = [
     {
-      icon: "assets/logo/wellbeing.png",
+      icon: "assets/images/logo/wellbeing.png",
       heading: "Safe Haven",
       message: "We offer a offer a secure and supportive space for healing, growth, and recovery."
     },
     {
-      icon: "assets/logo/behavioral_management.png",
+      icon: "assets/images/logo/behavioral_management.png",
       heading: "Behavioural Management",
       message: "We provide trauma-informed therapy, emotional support, and sessions with licensed therapists."
     },
     {
-      icon: "assets/logo/education.png",
+      icon: "assets/images/logo/education.png",
       heading: "Educational Support",
       message: "We provide homework help, school enrollment support, and personalized learning plans through assessments and education plans."
     },
     {
-      icon: "assets/logo/personalDevelopment.png",
+      icon: "assets/images/logo/personalDevelopment.png",
       heading: "Life Skills & Personal Development",
       message: "We offer financial literacy and budgeting workshops, life skills training, and career mentorship programs."
     },
     {
-      icon: "assets/logo/health.png",
+      icon: "assets/images/logo/health.png",
       heading: "Health & Wellness Support",
       message: "We offer health referrals, nutrition guidance, and wellness programs to support overall well-being."
     },
     {
-      icon: "assets/logo/family_reunification.png",
+      icon: "assets/images/logo/family_reunification.png",
       heading: "Family Reunification Services",
       message: "We provide supervised visitations, parenting support, and reunification planning for families."
     }
